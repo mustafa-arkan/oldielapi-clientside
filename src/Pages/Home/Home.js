16d5875a9ce74fa75a412798906e166da176a47f
mustafa-arkan <arkancse@gmail.com>
2022-11-26 22:37:07 +0600

    category start

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 
+
 import Banner from './Banner/Banner';
 import Services from './Services/Services';
 
@@ -10,8 +11,9 @@ const Home = () => {
     return (
         <div className='mx-5'>
            <Banner></Banner>
-
+           
            <Services></Services>
+           
           
          
         </div>
